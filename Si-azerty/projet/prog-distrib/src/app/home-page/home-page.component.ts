850f5e82d0e546a2ddf95543050a980bc7fe9006
Xeolia <alexia.serrierb@gmail.com>
2020-12-12 23:28:44 +0100

    set table

---
+++ Si-azerty/projet/prog-distrib/src/app/home-page/home-page.component.ts
@@ -0,0 +1,31 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  displayedColumns: string[] = ['aeroportdepart', 'destination', 'prix', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+export interface PeriodicElement {
+  destination: string;
+  aeroportdepart: string;
+  prix: number;
+  symbol: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {aeroportdepart: 'Paris CDG', destination: 'JFK', prix: 1.0079, symbol: 'H'},
+  {aeroportdepart: 'Detroit DTW', destination: 'CDG', prix: 4.0026, symbol: 'He'},
+  {aeroportdepart: 'Paris CDG', destination: 'DTW', prix: 6.941, symbol: 'Li'}
+];
+
