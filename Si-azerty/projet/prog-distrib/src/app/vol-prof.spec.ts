654fa47bbf82c29577ce67486cbc9d7b44d79ae2
Xeolia <alexia.serrierb@gmail.com>
2020-12-14 17:53:13 +0100

    config

---
+++ Si-azerty/projet/prog-distrib/src/app/vol-prof.spec.ts
@@ -0,0 +1,7 @@
+import { VolProf } from './vol-prof';
+
+describe('VolProf', () => {
+  it('should create an instance', () => {
+    expect(new VolProf()).toBeTruthy();
+  });
+});
---
+++ Si-azerty/projet/prog-distrib/src/app/vol-prof.ts
@@ -0,0 +1,6 @@
+export class VolProf {
+  code: string;
+  departure: string;
+  arrival: string;
+  base_price: number;
+}
